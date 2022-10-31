e47b0b708c35f3ec748830574f813f060460fe05
jjw213 <ssvsxx@naver.com>
2022-10-31 17:39:54 +0900

    초기 생성

---
+++ src/main/java/com/reactnativespring/rns/RnsApplication.java
@@ -0,0 +1,13 @@
+package com.reactnativespring.rns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactnativespring/rns/RnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactnativespring.rns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
