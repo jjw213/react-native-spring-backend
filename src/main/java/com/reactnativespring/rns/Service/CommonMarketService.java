260425e87cabb17f64f38c30b85b87869fd8ed2a
jjw213 <ssvsxx@naver.com>
2022-12-27 23:13:59 +0900

    service

--- src/main/java/com/reactnativespring/rns/Service/CommonMarketService.java
+++ src/main/java/com/reactnativespring/rns/Service/CommonMarketService.java
@@ -15,6 +15,7 @@ public class CommonMarketService {
     public double getPrice(String market, String coin) {
         MarketService marketService = null;
 
+
         for(String key : marketServices.keySet()) {
             if(key.substring(0, market.length()).equals(market.toLowerCase())) {
                 marketService = marketServices.get(key);
